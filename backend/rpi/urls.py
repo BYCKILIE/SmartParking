83f71b1f3c8b6672347b93e7e54bff497963be6f
Tudor Ovidiu Bichilie <tudorovidiub@gmail.com>
2024-05-14 18:01:54 +0300

    reformated

--- backend/rpi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('entry/', views.enter_parking),
-    path('leave/', views.leave_parking),
-    path('free-slot/', views.leave_slot),
-    path('cleanup/', views.cleanup)
-]
